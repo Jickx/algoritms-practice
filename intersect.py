43a054396f928d6f862df79a700ae27108867464
Jickx <63093696+Jickx@users.noreply.github.com>
2022-08-09 14:18:33 +0300

    O(n)

--- intersect.py
+++ intersect.py
@@ -14,19 +14,18 @@
 
 
 def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
-    dic_ctr1 = dict()
-    dic_ctr2 = dict()
+    dic = {}
     for i in nums1:
-        dic_ctr1[i] = dic_ctr1.get(i, 0) + 1
+        if i in dic:
+            dic[i] += 1
+        else:
+            dic[i] = 1
+    res = []
     for i in nums2:
-        dic_ctr2[i] = dic_ctr2.get(i, 0) + 1
-    output = []
-    for key in dic_ctr1:
-        if key not in dic_ctr2:
-            continue
-        quantity = min(dic_ctr1[key], dic_ctr2[key])
-        output.extend([key] * quantity)
-    return output
+        if i in dic and dic[i] > 0:
+            res.append(i)
+            dic[i] -= 1
+    return res
 
 
 assert intersect([1, 2, 2, 1], [2, 2]) == [2, 2]
